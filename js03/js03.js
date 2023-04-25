adc24f185c2c8ad0ec0304f1afce76072d5918fe
goyo2205 <jgregorio.ef@gmail.com>
2023-04-25 17:25:00 -0600

    ci: delcaracion bloque de codigo

--- js03/js03.js
+++ js03/js03.js
@@ -1,1 +1,19 @@
 console.log("Sesion JS03")
+
+
+/*---------------Declaracion de bloque*/
+{
+    /*
+    las variables declaradas con let y const solo tendran alcance dentro del bloque o bloques anidados
+     */
+    {
+        /*
+        bloque anidado
+        si una variable local tiene el mismo nombre de una variable local oculta las otras variables, 
+        para que la variable local sea usada
+        */
+    }
+}
+
+
+
