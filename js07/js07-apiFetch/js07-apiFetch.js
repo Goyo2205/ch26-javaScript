816291d9a5b3cde4c3c1a92814fc2e0d4193f2cd
goyo2205 <jgregorio.ef@gmail.com>
2023-05-04 17:37:17 -0600

    fetch

--- js07/js07-apiFetch/js07-apiFetch.js
+++ js07/js07-apiFetch/js07-apiFetch.js
@@ -1,1 +1,55 @@
 console.log('sesion js07  API Fetch')
+
+
+const url = "https://fakestoreapi.com/products";
+
+/* const getProducts = ( httpUrl ) =>{    
+
+    fetch( httpUrl )
+        .then( result => {
+            console.log("Respuesta: " + result.status );
+            return result.json(); // Conversión de JSON a objeto
+        })
+        .then ( (productsObj)=> {
+            console.log(productsObj)
+            // Quiero imprimir solo el titulo de cada producto.
+            productsObj.forEach(element => {
+                console.log(element.title);
+            });
+
+        })
+        .catch( error=> console.log(error)  );
+
+}
+
+getProducts( url ); */
+
+
+//--------Consumir Fetch Usando AWait---------------
+const getProductsWithAwait = async (httpUrl) => {
+    const productsJson = await fetch(httpUrl);
+    const products = await productsJson.json();
+    console.log(products);
+
+    for( let product of products) console.log(product.title);
+    console.error(error);
+}
+//getProductsWithAwait(url);
+
+//agragar try catch
+
+const getProductsWithAwaitTry = async (httpUrl) => {
+try {
+    const productsJson = await fetch(httpUrl);
+    const products = await productsJson.json();
+    console.log(products);
+
+    localStorage.setItem('mis-productos', JSON.stringify(products))//key, value
+
+    for( let product of products) console.log(product.title);
+
+} catch (error) {
+    console.error(error);
+}
+}
+getProductsWithAwaitTry(url);
