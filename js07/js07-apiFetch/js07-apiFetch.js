e414e31092696a337c496f231413327b985ec1f9
goyo2205 <jgregorio.ef@gmail.com>
2023-05-05 12:14:24 -0600

    api fetch

--- js07/js07-apiFetch/js07-apiFetch.js
+++ js07/js07-apiFetch/js07-apiFetch.js
@@ -1,55 +1,77 @@
-console.log('sesion js07  API Fetch')
+console.log("js 07 Api Fetch");
 
+/*
+ La Api Fetch nos ayuda a acceder y manipular peticiones HTTP. Proporciona
+ una forma lógica y fácil de obtener recursos de la red de manera ASÍNCRONA.
+
+Sintaxis:
+ Solicitud get
+
+ fetch(  url )
+  .then( )
+  .catch( );
+
+*/
 
 const url = "https://fakestoreapi.com/products";
 
-/* const getProducts = ( httpUrl ) =>{    
+const getProducts = (httpUrl) => {
+  fetch(httpUrl)
+    .then((result) => {
+      console.log("Respuesta: " + result.status);
+      return result.json(); // Conversión de JSON a objeto
+    })
+    .then((productsObj) => {
+      console.log(productsObj);
+      // Quiero imprimir solo el título de cada producto.
+      for (let product of productsObj) {
+        console.log(product.title);
+      }
+    })
+    .catch((error) => console.log(error));
+};
 
-    fetch( httpUrl )
-        .then( result => {
-            console.log("Respuesta: " + result.status );
-            return result.json(); // Conversión de JSON a objeto
-        })
-        .then ( (productsObj)=> {
-            console.log(productsObj)
-            // Quiero imprimir solo el titulo de cada producto.
-            productsObj.forEach(element => {
-                console.log(element.title);
-            });
+//getProducts( url );
 
-        })
-        .catch( error=> console.log(error)  );
+// --------------- Consumir Fetch usando async-await
 
-}
-
-getProducts( url ); */
-
-
-//--------Consumir Fetch Usando AWait---------------
 const getProductsWithAwait = async (httpUrl) => {
+  try {
     const productsJson = await fetch(httpUrl);
-    const products = await productsJson.json();
+    const products = await productsJson.json(); // Conversión de JSON a objeto
     console.log(products);
 
-    for( let product of products) console.log(product.title);
+    localStorage.setItem("mis-productos", JSON.stringify(  products)   ) // key, value
+                                     //Convertir a json
+    for (let product of products) console.log(product.title);
+  } catch (error) {
     console.error(error);
+  }
+};
+
+// getProductsWithAwait(url);
+
+// -------------- Realizar una solicitud POST ----------------------
+
+
+const postUser = ( url, userData) =>{
+
+  // fetch().then( ()=>{} ).catch( ()=>{} );
+  fetch( url, {
+    method: 'POST',  // PUT o DELETE
+    body:  JSON.stringify(userData)    , // Los datos del usuario, conversión Obj->JSON
+    headers : { 'Content-Type': 'application/json'}
+  })
+    .then( (response )=> response.json() )
+    .then( (register) => console.log( register )  )
+    .catch( (error)=>{ console.log(error)} );
 }
-//getProductsWithAwait(url);
 
-//agragar try catch
+const user = {
+    name: "El Iván",
+    job: "leader"
+};
 
-const getProductsWithAwaitTry = async (httpUrl) => {
-try {
-    const productsJson = await fetch(httpUrl);
-    const products = await productsJson.json();
-    console.log(products);
+const urlPOST = "https://reqres.in/api/users";
 
-    localStorage.setItem('mis-productos', JSON.stringify(products))//key, value
-
-    for( let product of products) console.log(product.title);
-
-} catch (error) {
-    console.error(error);
-}
-}
-getProductsWithAwaitTry(url);
+postUser( urlPOST, user );
