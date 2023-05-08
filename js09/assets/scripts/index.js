0f388418f8638317ba044db83876f22c2f786f96
goyo2205 <jgregorio.ef@gmail.com>
2023-05-08 12:21:59 -0600

    ci: axios con async y await

--- js09/assets/scripts/index.js
+++ js09/assets/scripts/index.js
@@ -1,1 +1,38 @@
 console.log('JS09 uso de axios')
+
+const url = 'https://reqres.in/api/users?page=2';
+
+const getUsersFetch = () => {
+    fetch(url).then(response => response.json())
+    .then( users => {
+        console.log(users.data)
+    }).catch(error => console.log(error))
+}
+
+/* const getUsersAxios = () => {r
+    
+    axios.get(url)
+        .then(users => {
+            console.log(users.data.data);
+        })
+        .catch(erro => console.log(error));
+} */
+
+
+// ahora con await
+/* const getUsersAxios = async() => {
+    const users = await axios.get(url);
+    console.log(users.data.data)
+};
+ */
+
+// ahora con try catch para el manejo de exepciones
+const getUsersAxios = async() => {
+    try{
+        const users = await axios.get(url);
+        console.log(users.data.data)
+    }
+    catch (error){
+        console.log(error)
+    }
+};
