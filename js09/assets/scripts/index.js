1e6a4b90e9571d196d6e04aa1e8f573fe132c29e
goyo2205 <jgregorio.ef@gmail.com>
2023-05-08 12:46:41 -0600

    se agrega post

--- js09/assets/scripts/index.js
+++ js09/assets/scripts/index.js
@@ -7,6 +7,7 @@ const getUsersFetch = () => {
     .then( users => {
         console.log(users.data)
     }).catch(error => console.log(error))
+    console.log('then')
 }
 
 /* const getUsersAxios = () => {r
@@ -35,4 +36,21 @@ const getUsersAxios = async() => {
     catch (error){
         console.log(error)
     }
+    console.log('await')
 };
+
+//-------solicitud post con axios
+
+const solicitudPost = async() => {
+    axios.post('https://reqres.in/api/users', {
+        firstName: "Goyo",
+        lastName: "Espinoza"
+    })
+    .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
